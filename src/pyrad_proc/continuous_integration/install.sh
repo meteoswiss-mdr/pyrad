f030b8f0b36d05bb2a974713dc06858d96dc4bf2
fvj <jordi.figuerasiventura@meteoswiss.ch>
2019-02-05 10:15:13 +0100

    test of Travis-ci

--- src/pyrad_proc/continuous_integration/install.sh
+++ src/pyrad_proc/continuous_integration/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
 # use next line to debug this script
 # set -x
 
 sudo apt-get update
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 
-bash miniconda.sh -b -p $HOME/miniconda
+chmod +x miniconda.sh
+./miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
