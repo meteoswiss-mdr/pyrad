2b81838d38f4ff6aeadf011b00232bd8272d9799
fvj <jordi.figuerasiventura@meteoswiss.ch>
2019-02-06 14:36:36 +0100

    test of github pages

--- doc/pyrad/make_pyrad_doc.sh
+++ doc/pyrad/make_pyrad_doc.sh
@@ -6,22 +6,33 @@
 # library reference for developers
 rm -f ../pyrad_library_reference_dev.pdf
 cd library_reference_developers
-make clean
 
 # make pdf
-# make latexpdf
-# cp build/latex/*.pdf ../../.
-# rm -rf build/*
-# cd ..
+make clean
+make latexpdf
+cp build/latex/*.pdf ../../.
+rm -rf build/*
+
+# make html for github pages
+# make clean
+# make html
+# rm -r ../../../docs/*
+# mv build/html/* ../../../docs
+cd ..
+
+# library reference for users
+rm -f ../pyrad_library_reference_users.pdf
+cd library_reference_users
+
+# make pdf
+make clean
+make latexpdf
+cp build/latex/*.pdf ../../.
+rm -rf build/*
+
+# make html for github pages
+make clean
 make html
 rm -r ../../../docs/*
 mv build/html/* ../../../docs
-
-# # library reference for users
-# rm -f ../pyrad_library_reference_users.pdf
-# cd library_reference_users
-# make clean
-# make latexpdf
-# cp build/latex/*.pdf ../../.
-# rm -rf build/*
-# cd ..
+cd ..
