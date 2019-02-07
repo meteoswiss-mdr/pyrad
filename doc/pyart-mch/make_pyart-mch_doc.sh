ec1bdfc33f1fd8dc6286e3e391bf255c32c23a9c
fvj <jordi.figuerasiventura@meteoswiss.ch>
2019-02-07 09:02:59 +0100

    new branches created in Py-ART

--- doc/pyart-mch/make_pyart-mch_doc.sh
+++ doc/pyart-mch/make_pyart-mch_doc.sh
@@ -6,17 +6,37 @@
 # library reference for developers
 rm -f ../pyart-mch_library_reference_dev.pdf
 cd library_reference_developers
-make clean
-make latexpdf
-cp build/latex/*.pdf ../../.
-rm -rf build/*
+
+# make pdf
+# make clean
+# make latexpdf
+# cp build/latex/*.pdf ../../.
+# rm -rf build/*
+
+# make html for github pages
+# make clean
+# make html
+# rm -r ../../../docs/*
+# mv build/html/* ../../../docs
+# rm -rf build/*
 cd ..
 
 # library reference for users
 rm -f ../pyart-mch_library_reference_users.pdf
 cd library_reference_users
+
+# # make pdf
+# make clean
+# make latexpdf
+# cp build/latex/*.pdf ../../.
+# rm -rf build/*
+# cd ..
+
+# make html for github pages
 make clean
-make latexpdf
-cp build/latex/*.pdf ../../.
+make html
+git rm -r ../../../src/pyart/docs/*
+mv build/html/* ../../../src/pyart/docs
 rm -rf build/*
+touch ../../../src/pyart/docs/.nojekyll
 cd ..
